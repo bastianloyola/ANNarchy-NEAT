68223d3e778bf0972ed8bc33d70f2fc07967e0c1
Gabriela Fernández Rodríguez <gabriela.fernandez.r@usach.cl>
2024-03-21 18:50:37 -0300

    Conexión a Python

---
+++ NEAT/annarchy.py
@@ -0,0 +1,60 @@
+from ANNarchy import *
+import matplotlib.pyplot as plt
+
+def neuralNetwork(): 
+    print("1")
+    pop = Population(geometry=1000, neuron=Izhikevich)
+    excSize = int(800)
+    Exc = pop[:800]
+    Inh = pop[800:]
+    print("2")
+
+    re = np.random.random(800)  ; ri = np.random.random(200)
+    Exc.noise = 5.0                 ; Inh.noise = 2.0
+    Exc.a = 0.02                    ; Inh.a = 0.02 + 0.08 * ri
+    Exc.b = 0.2                     ; Inh.b = 0.25 - 0.05 * ri
+    Exc.c = -65.0 + 15.0 * re**2    ; Inh.c = -65.0
+    Exc.d = 8.0 - 6.0 * re**2       ; Inh.d = 2.0
+    Exc.v = -65.0                   ; Inh.v = -65.0
+    Exc.u = Exc.v * Exc.b           ; Inh.u = Inh.v * Inh.b
+    print("3")
+    exc_proj = Projection(pre=Exc, post=pop, target='exc')
+    exc_proj.connect_all_to_all(weights=Uniform(0.0, 0.5))
+
+    inh_proj = Projection(pre=Inh, post=pop, target='inh')
+    inh_proj.connect_all_to_all(weights=Uniform(0.0, 1.0))
+
+    print("4")
+    compile()
+    print("5")
+    M = Monitor(pop, ['spike', 'v'])
+
+    simulate(1000.0, measure_time=True)
+    print("6")
+    spikes = M.get('spike')
+    v = M.get('v')
+    t, n = M.raster_plot(spikes)
+    fr = M.histogram(spikes)
+    print("7")
+
+    fig = plt.figure(figsize=(12, 12))
+
+    # First plot: raster plot
+    plt.subplot(311)
+    plt.plot(t, n, 'b.')
+    plt.title('Raster plot')
+
+    # Second plot: membrane potential of a single excitatory cell
+    plt.subplot(312)
+    plt.plot(v[:, 15]) # for example
+    plt.title('Membrane potential')
+
+    # Third plot: number of spikes per step in the population.
+    plt.subplot(313)
+    plt.plot(fr)
+    plt.title('Number of spikes')
+    plt.xlabel('Time (ms)')
+
+    plt.tight_layout()
+    plt.show()
+    return 0
--- NEAT/main.cpp
+++ NEAT/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <vector>
 
+#define PY_SSIZE_T_CLEAN
+//revisar
+//#include <Python.h>
+#include </Library/Frameworks/Python.framework/Versions/3.12/include/python3.12/Python.h>
+
 #include "node.h"
 #include "connection.h"
 #include "genome.h"
@@ -9,8 +14,33 @@
 
 using namespace std;
 
+void runPy(){
+  setenv("PYTHONPATH", ".", 1);
+  Py_Initialize();
+  PyObject *name, *load_module, *func, *callfunc, *args;
+  name = PyUnicode_FromString("annarchy");
+  load_module = PyImport_Import(name);
+
+  func = PyObject_GetAttrString(load_module, (char*)"neuralNetwork");
+  //args = PyTuple_Pack(1, PyFloat_FromDouble(population_size));
+  callfunc = PyObject_CallObject(func,NULL);
+  double out = PyFloat_AsDouble(callfunc);
+  
+  cout << out << endl;
+  cout << out << endl;
+
+  Py_DECREF(name);
+  Py_DECREF(load_module);
+  Py_DECREF(func);
+  Py_XDECREF(callfunc);
+  Py_XDECREF(args);
+
+  Py_Finalize();
+}
+
 int main() {
-    //testing_all_classes_and_methods();
-    menu();
+  runPy();
+  //testing_all_classes_and_methods();
+  //menu();
   return 0;
 }
