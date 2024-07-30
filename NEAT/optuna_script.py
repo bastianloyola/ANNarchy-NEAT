22192de123d4f143889618bc503f5c442cb8f3c4
Gabriela Fernandez Rodriguez <gabriela.fernandez.r@usach.cl>
2024-07-29 20:56:21 -0400

    fix

--- NEAT/optuna_script.py
+++ NEAT/optuna_script.py
@@ -1,79 +1,77 @@
-import optuna
+import optuna 
+import sys
+import logging
 import subprocess
 import numpy as np
 
-# Función para modificar config.cfg
-def modify_config(params):
-    with open('config.cfg', 'w') as f:
-        for key, value in params.items():
-            f.write(f'{key}={value}\n')
+from optuna.visualization import plot_contour
+from optuna.visualization import plot_edf
+from optuna.visualization import plot_intermediate_values
+from optuna.visualization import plot_optimization_history
+from optuna.visualization import plot_parallel_coordinate
+from optuna.visualization import plot_param_importances
+from optuna.visualization import plot_slice
+
+
 
 # Función de objetivo para Optuna
 def objective(trial):
+    # Trial: single execution of the objective function
+    # Suggest call parameters uniformly within the range 
     # Definir los hiperparámetros que Optuna debe optimizar
-    keep = trial.suggest_float('keep', 0.0, 1.0)
-    threshold = trial.suggest_float('threshold', 0.0, 10.0)
-    probabilityInterespecies = trial.suggest_float('probabilityInterespecies', 0.0, 0.01)
-    probabilityNoCrossoverOff = trial.suggest_float('probabilityNoCrossoverOff', 0.0, 1.0)
-    probabilityWeightMutated = trial.suggest_float('probabilityWeightMutated', 0.0, 1.0)
-    probabilityAddNodeSmall = trial.suggest_float('probabilityAddNodeSmall', 0.0, 0.1)
-    probabilityAddLink_small = trial.suggest_float('probabilityAddLink_small', 0.0, 0.1)
-    probabilityAddNodeLarge = trial.suggest_float('probabilityAddNodeLarge', 0.0, 0.1)
-    probabilityAddLink_Large = trial.suggest_float('probabilityAddLink_Large', 0.0, 1.0)
-    largeSize = trial.suggest_int('largeSize', 1, 100)  # Cambiado a entero
-    c1 = trial.suggest_float('c1', 0.0, 10.0)
-    c2 = trial.suggest_float('c2', 0.0, 10.0)
-    c3 = trial.suggest_float('c3', 0.0, 1.0)
+    keep = trial.suggest_float('keep', 0.1, 0.7)
+    threshold = trial.suggest_float('threshold', 2.0, 5.0)
+    interespeciesRate = trial.suggest_float('probabilityInterespecies', 0.01, 0.5)
+    noCrossoverOff = trial.suggest_float('probabilityNoCrossoverOff', 0.2, 0.6)
+    probabilityWeightMutated = trial.suggest_float('probabilityWeightMutated', 0.4, 0.9)
+    probabilityAddNodeSmall = trial.suggest_float('probabilityAddNodeSmall', 0.01, 0.1)
+    probabilityAddLink_small = trial.suggest_float('probabilityAddLink_small', 0.01, 0.1)
+    probabilityAddNodeLarge = trial.suggest_float('probabilityAddNodeLarge', 0.01, 0.1)
+    probabilityAddLink_Large = trial.suggest_float('probabilityAddLink_Large', 0.1, 0.4)
+    c1 = trial.suggest_float('c1', 0.1, 3.0)
+    c2 = trial.suggest_float('c2', 0.1, 3.0)
+    c3 = trial.suggest_float('c3', 0.1, 3.0)
+    p = subprocess.Popen(["./NEAT", str(keep), str(threshold), str(interespeciesRate), str(noCrossoverOff), str(probabilityWeightMutated), str(probabilityAddNodeSmall), str(probabilityAddLink_small), str(probabilityAddNodeLarge), str(probabilityAddLink_Large), str(c1), str(c2), str(c3)],
+                        stderr=subprocess.PIPE, 
+                        stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
-    # Mantener los parámetros que no deben cambiar
-    params = {
-        'keep': keep,
-        'threshold': threshold,
-        'probabilityInterespecies': probabilityInterespecies,
-        'probabilityNoCrossoverOff': probabilityNoCrossoverOff,
-        'probabilityWeightMutated': probabilityWeightMutated,
-        'probabilityAddNodeSmall': probabilityAddNodeSmall,
-        'probabilityAddLink_small': probabilityAddLink_small,
-        'probabilityAddNodeLarge': probabilityAddNodeLarge,
-        'probabilityAddLink_Large': probabilityAddLink_Large,
-        'largeSize': largeSize,
-        'c1': c1,
-        'c2': c2,
-        'c3': c3,
-        'initial_weight': 30.0,  # No cambiar
-        'numberGenomes': 2,  # No cambiar
-        'numberInputs': 80,  # No cambiar
-        'numberOutputs': 40,  # No cambiar
-        'evolutions': 3,  # No cambiar
-        'n_max': 200,  # No cambiar
-        'learningRate': 10.0,  # No cambiar
-        'inputWeights': '0.0,150.0',  # No cambiar
-        'weightsRange': '-20.0,80.0',  # No cambiar
-        'process_max': 2,  # No cambiar
-        'function': 'cartpole2'  # No cambiar
-    }
-    modify_config(params)
 
-    # Llamar a la función main desde C++
-    result = subprocess.run(['./NEAT'], capture_output=True, text=True)
+    p.wait()
+    FuncValue = p.returncode
 
-    # Verificar si hubo algún error en la ejecución
-    if result.returncode != 0:
-        print(f"Error: {result.stderr}")
-        return float('inf')  # Devolver un valor alto en caso de error
+    #get final line of output
+    if FuncValue != None:
+        FuncValue = float(FuncValue)
+    else:
+        FuncValue = -np.inf
 
-    # Extraer el resultado de la salida del programa
-    try:
-        fitness = float(result.stdout.strip())
-    except ValueError:
-        print("Error: No se pudo convertir la salida a float.")
-        return float('inf')  # Devolver un valor alto en caso de error
+    return FuncValue
+# Add stream handler of stdout to show the messages
+optuna.logging.get_logger("optuna").addHandler(logging.StreamHandler(sys.stdout))
 
-    return fitness
-
-# Crear y ejecutar el estudio de Optuna
-study = optuna.create_study(direction='maximize')
-study.optimize(objective, n_trials=100)
+# Create the study object (an optimization session = set of trials)
+study = optuna.create_study(study_name='Test-study',
+                            direction='maximize', 
+                            sampler=optuna.samplers.TPESampler(),
+                            pruner=optuna.pruners.HyperbandPruner(),
+                            load_if_exists=True)
+# Pass the objective function method
+study.optimize(objective, n_trials=100, timeout=18000) #timeout in seconds
 
 print(f'Mejor valor: {study.best_value}')
 print(f'Mejores parámetros: {study.best_params}')
+
+# Get the best parameter
+found_params = study.best_params
+found_value  = study.best_value
+found_trial  = study.best_trial
+
+# Visualization options 
+fig = optuna.visualization.plot_optimization_history(study)
+fig = optuna.visualization.plot_parallel_coordinate(study)
+fig = optuna.visualization.plot_slice(study)
+fig = optuna.visualization.plot_param_importances(study)
+fig = optuna.visualization.plot_edf(study)
+fig.show()
+
+#https://adambaskerville.github.io/posts/PythonSubprocess
--- NEAT/src/main.cpp
+++ NEAT/src/main.cpp
@@ -29,17 +29,18 @@ int main(int argc, char *argv[]) {
   float c3=std::atof(argv[12]);
 
   // Parametros constantes
-  int numberGenomes=50;
-  int numberInputs=8;
-  int numberOutputs=2;
-  int evolutions=50;
+  float initial_weights=110.0;
+  int numberGenomes=10;
+  int numberInputs=2;
+  int numberOutputs=1;
+  int evolutions=10;
   float learningRate=10.0;
-  float inputWeights_min=0.0;
+  float inputWeights_min=110.0;
   float inputWeights_max=150.0;
   float weightsRange_min=-20.0;
   float weightsRange_max=80.0;
   int process_max=2;
-  string function="cartpole";
+  string function="xor";
 
   // Escribir en el archivo config.cfg
   ofstream config_file("config/config.cfg", ofstream::trunc);
@@ -49,8 +50,8 @@ int main(int argc, char *argv[]) {
   }
   config_file << "keep=" << keep << "\n";
   config_file << "threshold=" << threshold << "\n";
-  config_file << "probabilityInterespecies=" << probabilityInterespecies << "\n";
-  config_file << "probabilityNoCrossoverOff=" << probabilityNoCrossoverOff << "\n";
+  config_file << "interespeciesRate=" << probabilityInterespecies << "\n";
+  config_file << "noCrossoverOff=" << probabilityNoCrossoverOff << "\n";
   config_file << "probabilityWeightMutated=" << probabilityWeightMutated << "\n";
   config_file << "probabilityAddNodeSmall=" << probabilityAddNodeSmall << "\n";
   config_file << "probabilityAddLink_small=" << probabilityAddLink_small << "\n";
@@ -60,7 +61,7 @@ int main(int argc, char *argv[]) {
   config_file << "c1=" << c1 << "\n";
   config_file << "c2=" << c2 << "\n";
   config_file << "c3=" << c3 << "\n";
-  config_file << "initial_weight=30.0\n";
+  config_file << "initial_weight=" << initial_weights << "\n";
   config_file << "numberGenomes=" << numberGenomes << "\n";
   config_file << "numberInputs=" << numberInputs << "\n";
   config_file << "numberOutputs=" << numberOutputs << "\n";
