aeacd110388c5da320e33034fa66c60ae044e80e
Florencia Corvalán <florencia.corvalan@usach.cl>
2024-09-13 00:46:58 -0300

    Modificaciones para ejecutar con entorno virtual de python

--- NEAT/src/menu.cpp
+++ NEAT/src/menu.cpp
@@ -1,6 +1,6 @@
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 #include "../headers/menu.h"
-#include <python3.10/numpy/arrayobject.h>
+#include <numpy/arrayobject.h>
 #include <iostream>
 #include <vector>
 
